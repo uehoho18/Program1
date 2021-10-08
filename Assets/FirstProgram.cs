6f0012672bb3e65c321bb3093e3da80bcc967242
tomohiroueno <ramune276@gmail.com>
2021-10-08 10:49:21 +0900

    制御構文2

--- Assets/FirstProgram.cs
+++ Assets/FirstProgram.cs
@@ -4,32 +4,18 @@ using UnityEngine;
 
 public class FirstProgram : MonoBehaviour
 {
-    // 変数の形
-    // データ型
-
-    // 定数
-    //                                 0    1    2
-    int[] stage_points = new int[3] {1000,1200,1300};
-
-    List<int> stage_pointList = new List<int>();
-
-    int mario_point = 0;
-
-    int stage = 1;
+    int number = 10;
 
     void Start()
     {
+        int i = 0;
+        while (number < 20) {
 
-        stage_pointList.Add(1000);
-        stage_pointList.Add(1200);
-        stage_pointList.Add(1300);
-        //3つのリスト
+            Debug.Log(""+number);
 
-        stage_pointList.Remove(1200);
-        //2つのリスト
-
-        stage_pointList.Clear();
-        //0つのリスト
-
+            i++;
+            if(i>=2)break;
+        }
+        
     }
 }
